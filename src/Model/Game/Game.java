c60661a8ddaa72463c42c06006678a374d6ce170
Hellyon <ilyace456b@gmail.com>
2020-04-10 15:42:57 +0200

    var name refactor

--- src/Model/Game/Game.java
+++ src/Model/Game/Game.java
@@ -32,11 +32,11 @@ public class Game {
 
             // Initialisation of variables
             AbstractTerrain terrain = new Terrain(20, 20);
-            LinkedList<Agent> agentsTeam1 = new LinkedList<>();
-            LinkedList<Agent> agentsTeam2 = new LinkedList<>();
+            LinkedList<Agent> blueTeam = new LinkedList<>();
+            LinkedList<Agent> redTeam = new LinkedList<>();
 
-            int teamSize1 = TEAM_SIZE, teamSize2 = TEAM_SIZE;
-            Agent currentAgentTeam1, currentAgentTeam2;
+            int blueTeamSize = TEAM_SIZE, redTeamSize = TEAM_SIZE;
+            Agent currentBlueTeamAgent, currentRedTeamAgent;
 
             // Random variates
             int type = 0;
@@ -51,16 +51,16 @@ public class Game {
                 // Switch the type of Agent
                 switch (type) {
                     case 0:
-                        agentsTeam1.add(factory.create(AgentType.AXEMAN, ANSI_RED));
+                        blueTeam.add(factory.create(AgentType.AXEMAN, ANSI_RED));
                         break;
                     case 1:
-                        agentsTeam1.add(factory.create(AgentType.BOWMAN, ANSI_RED));
+                        blueTeam.add(factory.create(AgentType.BOWMAN, ANSI_RED));
                         break;
                     case 2:
-                        agentsTeam1.add(factory.create(AgentType.KNIGHT, ANSI_RED));
+                        blueTeam.add(factory.create(AgentType.KNIGHT, ANSI_RED));
                         break;
                     case 3:
-                        agentsTeam1.add(factory.create(AgentType.LANCER, ANSI_RED));
+                        blueTeam.add(factory.create(AgentType.LANCER, ANSI_RED));
                         break;
                 }
             }
@@ -72,75 +72,72 @@ public class Game {
                 // Switch the type of Agent
                 switch (type) {
                     case 0:
-                        agentsTeam2.add(factory.create(AgentType.AXEMAN, ANSI_BLUE));
+                        redTeam.add(factory.create(AgentType.AXEMAN, ANSI_BLUE));
                         break;
                     case 1:
-                        agentsTeam2.add(factory.create(AgentType.BOWMAN, ANSI_BLUE));
+                        redTeam.add(factory.create(AgentType.BOWMAN, ANSI_BLUE));
                         break;
                     case 2:
-                        agentsTeam2.add(factory.create(AgentType.KNIGHT, ANSI_BLUE));
+                        redTeam.add(factory.create(AgentType.KNIGHT, ANSI_BLUE));
                         break;
                     case 3:
-                        agentsTeam2.add(factory.create(AgentType.LANCER, ANSI_BLUE));
+                        redTeam.add(factory.create(AgentType.LANCER, ANSI_BLUE));
                         break;
                 }
             }
 
             // We place the teams on the board
-            terrain.placeAgents(agentsTeam1);
-            terrain.placeAgents(agentsTeam2);
+            terrain.placeAgents(blueTeam);
+            terrain.placeAgents(redTeam);
             terrain.showTerrain();
             System.out.println("\n");
 
-            Iterator<Agent> itTeam1 = agentsTeam1.iterator();
-            Iterator<Agent> itTeam2 = agentsTeam2.iterator();
+            Iterator<Agent> blueTeamIterator = blueTeam.iterator();
+            Iterator<Agent> redTeamIterator = redTeam.iterator();
 
-            for(int i = 0; i < 100 && (teamSize1 > 0 || teamSize2 > 0); ++i)
+            for(int i = 0; i < 100 && (blueTeamSize > 0 && redTeamSize > 0); ++i)
             {
                 System.out.println(" \n Iteration" + i);
-                while(itTeam1.hasNext() && itTeam2.hasNext())
+                while(blueTeamIterator.hasNext() && redTeamIterator.hasNext())
                 {
-                    currentAgentTeam1 = itTeam1.next();
-                    if(teamSize2 > 0 && currentAgentTeam1.isAlive()){
-                        switch (currentAgentTeam1.actionTurn(terrain, agentsTeam2, agentsTeam1)){
+                    currentBlueTeamAgent = blueTeamIterator.next();
+                    if(redTeamSize > 0 && currentBlueTeamAgent.isAlive()){
+                        switch (currentBlueTeamAgent.actionTurn(terrain, redTeam, blueTeam)){
                             case LOST:
-                                teamSize1-=1;
+                                blueTeamSize-=1;
                                 break;
                             case WIN:
-                                teamSize2-=1;
+                                redTeamSize-=1;
                                 break;
                         }
                     }
 
-                    currentAgentTeam2 = itTeam2.next();
-                    if(teamSize1 > 0 && currentAgentTeam2.isAlive())
+                    currentRedTeamAgent = redTeamIterator.next();
+                    if(blueTeamSize > 0 && currentRedTeamAgent.isAlive())
                     {
-                        switch (currentAgentTeam2.actionTurn(terrain, agentsTeam1, agentsTeam2)){
+                        switch (currentRedTeamAgent.actionTurn(terrain, blueTeam, redTeam)){
                             case LOST:
-                                teamSize2-=1;
+                                redTeamSize-=1;
                                 break;
                             case WIN:
-                                teamSize1-=1;
+                                blueTeamSize-=1;
                                 break;
                         }
-                    }
-                    else {
-                        System.out.println();
                     }
                     //System.out.println("\n");
                 }
                 terrain.showTerrain();
                 System.out.println();
-                itTeam1 = agentsTeam1.iterator();
-                itTeam2 = agentsTeam2.iterator();
+                blueTeamIterator = blueTeam.iterator();
+                redTeamIterator = redTeam.iterator();
 
             }
 
-            if(teamSize1 == 0){
-                System.out.println("Victoire de l'équipe 1");
+            if(blueTeamSize == 0){
+                System.out.println("Victoire de l'équipe Bleu");
             }
             else{
-                System.out.println("Victoire de l'équipe 2");
+                System.out.println("Victoire de l'équipe Rouge");
             }
         } catch (Exception e) {
             System.err.println("[Exception]");
